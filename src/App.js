680d657b377fed97e9df831639d035859ca4453f
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-01-31 15:58:20 +0530

    created react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+// import Navbar from './Component/Navbar';
+// import Quora from './Component/Quora';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+{/* <h1 className='red'>
+  Note: this is only Quora Clone not original!</h1>  */}
+  {/* <Quora/>   */}
+
+
+  Ty Jesus for this MERN Quora Stack
+   </div>
   );
 }
 
 export default App;
+
